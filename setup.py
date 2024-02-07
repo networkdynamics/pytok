92dc6d39640e13b18af6a4c1dd9e759f3f2a006f
Ben Steel <bendavidsteel@gmail.com>
2024-02-07 22:40:36 +0000

    Updated install requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=["tiktok", "python3", "api", "unofficial", "tiktok-api", "tiktok api"],
-    install_requires=["requests", "playwright", "undetected-playwright", "pyvirtualdisplay", "tqdm"],
+    install_requires=["requests", "playwright", "playwright_stealth", "pyvirtualdisplay", "tqdm"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
